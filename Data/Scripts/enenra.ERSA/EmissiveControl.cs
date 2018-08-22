8ff1f39bcc625b7938ed4eade55b3592738b2d21
enenra <enenra9@gmail.com>
2018-08-23 00:20:38 +0200

    testing adjusting the script for the research block

--- Data/Scripts/enenra.ERSA/EmissiveControl.cs
+++ Data/Scripts/enenra.ERSA/EmissiveControl.cs
@@ -1,67 +1,91 @@
 using VRage.Game.Components;
 using Sandbox.ModAPI;
-using VRage.Game;
 using VRage.ObjectBuilders;
 using VRage.ModAPI;
-using VRage.Game.ModAPI;
 using VRageMath;
+using Sandbox.Common.ObjectBuilders.Definitions;
 
 namespace enenra.EmissiveControl
 {
-	[MyEntityComponentDescriptor(typeof(MyObjectBuilder_CargoContainer), false,
-		new string[]
-		{
-			"ERSA_LG_DataStorage"
-		}
-	)]
-	public class Container : MyGameLogicComponent
-	{
-		IMyCubeBlock m_block;
+    [MyEntityComponentDescriptor(typeof(MyObjectBuilder_AssemblerDefinition), false,
+        new string[]
+        {
+            "ERSA_LG_ResearchLab"
+        }
+    )]
+    public class Container : MyGameLogicComponent
+    {
+        Sandbox.ModAPI.IMyAssembler m_block;
 
-		private string EMISSIVE_MATERIAL_NAME = "Emissive";
-		private Color GREEN = new Color(0, 240, 0);
-		private Color RED = new Color(240, 0, 0);
+        private string EMISSIVE_MATERIAL_NAME = "Emissive";
+        private Color GREEN = new Color(0, 240, 0);
+        private Color BLUE = new Color(0, 0, 240);
+        private Color RED = new Color(240, 0, 0);
 
 
         bool IsWorking
-		{
-			get
-		{
-			return m_block.IsWorking;
-		}
-		}
+        {
+            get
+            {
+                return m_block.IsWorking;
+            }
+        }
 
-			public override MyObjectBuilder_EntityBase GetObjectBuilder(bool copy = false)
-		{
-			return Container.Entity.GetObjectBuilder(copy);
-		}
+        bool IsResearching
+        {
+            get
+            {
+                return m_block.IsProducing;
+            }
+        }
 
-		public override void Init(MyObjectBuilder_EntityBase objectBuilder)
-		{
-			NeedsUpdate |= MyEntityUpdateEnum.EACH_10TH_FRAME;
+        public override MyObjectBuilder_EntityBase GetObjectBuilder(bool copy = false)
+        {
+            return Container.Entity.GetObjectBuilder(copy);
+        }
 
-			m_block = (IMyCubeBlock)Entity;
-		}
+        public override void Init(MyObjectBuilder_EntityBase objectBuilder)
+        {
+            NeedsUpdate |= MyEntityUpdateEnum.EACH_10TH_FRAME;
 
-		public override void Close()
-		{
-			NeedsUpdate |= MyEntityUpdateEnum.NONE;
-			m_block = null;
-		}
+            m_block = (Sandbox.ModAPI.IMyAssembler)Entity;
+        }
 
-		public override void UpdateAfterSimulation10()
-		{
-			if (MyAPIGateway.Session == null)
-				return;
+        public override void Close()
+        {
+            NeedsUpdate |= MyEntityUpdateEnum.NONE;
+            m_block = null;
+        }
 
-			if (MyAPIGateway.Utilities.IsDedicated && MyAPIGateway.Multiplayer.IsServer)
-				return;
+        public override void UpdateAfterSimulation10()
+        {
+            if (MyAPIGateway.Session == null)
+                return;
 
-			if (IsWorking)
-				m_block.SetEmissiveParts(EMISSIVE_MATERIAL_NAME, GREEN, 1f);
-			else
-				m_block.SetEmissiveParts(EMISSIVE_MATERIAL_NAME, RED, 1f);
+            if (MyAPIGateway.Utilities.IsDedicated && MyAPIGateway.Multiplayer.IsServer)
+                return;
 
-		}
-	}
+            if (IsWorking)
+            {
+                if (IsResearching)
+                {
+                    m_block.SetEmissiveParts(EMISSIVE_MATERIAL_NAME, BLUE, 1f);
+                    MyAPIGateway.Utilities.ShowMessage("enenra", "blue");
+                }
+
+                else
+                {
+                    m_block.SetEmissiveParts(EMISSIVE_MATERIAL_NAME, GREEN, 1f);
+                    MyAPIGateway.Utilities.ShowMessage("enenra", "green");
+                }
+            }
+
+            else
+            {
+                m_block.SetEmissiveParts(EMISSIVE_MATERIAL_NAME, RED, 1f);
+                MyAPIGateway.Utilities.ShowMessage("enenra", "red");
+            }
+
+        }
+    }
 }

dd872a1ecedff245c36815d38d1621fda58c6ee7
enenra <enenra9@gmail.com>
2018-08-22 22:18:08 +0200

    Script for controlling the emissives on the DataStorage block.
    Doesn't work yet.

---
+++ Data/Scripts/enenra.ERSA/EmissiveControl.cs
@@ -0,0 +1,68 @@
+using VRage.Game.Components;
+using Sandbox.ModAPI;
+using VRage.Game;
+using VRage.ObjectBuilders;
+using VRage.ModAPI;
+using VRage.Game.ModAPI;
+using VRageMath;
+
+namespace enenra.EmissiveControl
+{
+	[MyEntityComponentDescriptor(typeof(MyObjectBuilder_CargoContainer), false,
+		new string[]
+		{
+			"ERSA_LG_DataStorage"
+		}
+	)]
+	public class Container : MyGameLogicComponent
+	{
+		IMyCubeBlock m_block;
+
+		private string EMISSIVE_MATERIAL_NAME = "Emissive";
+		private Color GREEN = new Color(0, 240, 0);
+		private Color RED = new Color(240, 0, 0);
+        MyAPIGateway.Utilities.ShowMessage("enenra","test");
+
+
+        bool IsWorking
+		{
+			get
+		{
+			return m_block.IsWorking;
+		}
+		}
+
+			public override MyObjectBuilder_EntityBase GetObjectBuilder(bool copy = false)
+		{
+			return Container.Entity.GetObjectBuilder(copy);
+		}
+
+		public override void Init(MyObjectBuilder_EntityBase objectBuilder)
+		{
+			NeedsUpdate |= MyEntityUpdateEnum.EACH_10TH_FRAME;
+
+			m_block = (IMyCubeBlock)Entity;
+		}
+
+		public override void Close()
+		{
+			NeedsUpdate |= MyEntityUpdateEnum.NONE;
+			m_block = null;
+		}
+
+		public override void UpdateAfterSimulation10()
+		{
+			if (MyAPIGateway.Session == null)
+				return;
+
+			if (MyAPIGateway.Utilities.IsDedicated && MyAPIGateway.Multiplayer.IsServer)
+				return;
+
+			if (IsWorking)
+				m_block.SetEmissiveParts(EMISSIVE_MATERIAL_NAME, GREEN, 1f);
+			else
+				m_block.SetEmissiveParts(EMISSIVE_MATERIAL_NAME, RED, 1f);
+
+		}
+	}
+}

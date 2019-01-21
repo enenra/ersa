02dbe630c374d6bd9a554c23b57566a139b57902
enenra <enenra9@gmail.com>
2019-01-21 23:10:46 +0100

    Revert "Revert "Some further adjustments to material prices""
    This reverts commit ba2884b6832a7f32366c083da4d9dee43a897a2a.

---
+++ Data/Scripts/enenra.ERSA/InventoryBlacklist.cs
@@ -0,0 +1,48 @@
+﻿using VRage.Game.Components;
+using Sandbox.ModAPI;
+using VRage.ObjectBuilders;
+using VRage.ModAPI;
+using VRageMath;
+using Sandbox.Common.ObjectBuilders;
+
+namespace enenra.InventoryBlacklist
+{
+    [MyEntityComponentDescriptor(typeof(MyObjectBuilder_Assembler), false,
+        new string[]
+        {
+            "ERSA_LG_ResearchLab"
+        }
+    )]
+    public class Container : MyGameLogicComponent
+    {
+        Sandbox.ModAPI.IMyAssembler m_block;
+
+        public override MyObjectBuilder_EntityBase GetObjectBuilder(bool copy = false)
+        {
+            return Container.Entity.GetObjectBuilder(copy);
+        }
+
+        public override void Init(MyObjectBuilder_EntityBase objectBuilder)
+        {
+            NeedsUpdate |= MyEntityUpdateEnum.EACH_10TH_FRAME;
+
+            m_block = (Sandbox.ModAPI.IMyAssembler)Entity;
+        }
+
+        public override void Close()
+        {
+            NeedsUpdate |= MyEntityUpdateEnum.NONE;
+            m_block = null;
+        }
+
+        public override void UpdateAfterSimulation10()
+        {
+            if (MyAPIGateway.Session == null)
+                return;
+
+            if (MyAPIGateway.Utilities.IsDedicated && MyAPIGateway.Multiplayer.IsServer)
+                return;
+
+        }
+    }
+}
